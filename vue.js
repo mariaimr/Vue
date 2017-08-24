ea37a64dc477885a779f747f7cd0634074e8d84b
mariaimr <mariaimr0694@gmail.com>
2017-08-23 19:31:13 -0500

    renderizar listas

--- vue.js
+++ vue.js
@@ -1,8 +1,24 @@
 //Vue.js
-const vm= new Vue({
+/*const vm= new Vue({
     el: 'main',
     data: {
         conectado: false,
         edad: 44,
     }
+});*/
+
+const vm= new Vue({
+    el: 'main',
+    data: {
+        laborales: ['lunes','martes','miercoles','jueves','viernes'],
+        tareas:[{nombre: 'x', edad: 23},
+                {nombre: 'y', edad: 24},
+                {nombre: 'z', edad: 25},
+              ],
+        persona: {
+          nombre: 'Maria',
+          apellido: 'Martinez',
+          ciudad: 'Medellin'
+        }
+    },
 });
